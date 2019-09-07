60a81036b06cb45786895a0868b330bbaae0ac61
Marvin Wendt <github@marvinjwendt.com>
2019-09-07 04:17:46 +0200

    Added yarn support

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,17 @@
 
 set -e
 
-npm install
-npm run lint
+echo "## Your environment is not ready yet. Installing modules..."
+if [ -f yarn.lock ]; then
+    yarn --non-interactive --silent --ignore-scripts --production=false
+    yarn install
+    yarn run lint
+else
+    NODE_ENV=development 
+    npm install
+    npm run lint
+fi
+
 git add *
 git commit -m "Linted code"
 git push
