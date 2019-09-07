937b64f34ad1c65009b697f0280590e2ab2f4873
Marvin Wendt <github@marvinjwendt.com>
2019-09-07 05:20:27 +0200

    Add more output

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,19 +7,29 @@ REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
 echo "## Your environment is not ready yet. Installing modules..."
 if [ -f yarn.lock ]; then
     yarn --non-interactive --silent --ignore-scripts --production=false
+    echo "## Installing dependencies..."
     yarn install
+    echo "## Linting code..."
     yarn run lint
 else
+    echo "## Setting environment variables..."
     NODE_ENV=development 
+    echo "## Installing dependencies..."
     npm install
+    echo "## Linting code..."
     npm run lint
 fi
 
+echo "## Login into git..."
 git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
 git config --global user.email "linter@1337z.ninja"
 git config --global user.name "Node Code Linter"
 
-git add .
+echo "## Deleting node_modules..."
 rm -rf node_modules/
+echo "## Staging changes..."
+git add .
+echo "## Commiting files..."
 git commit -m "Linted code"
+echo "## Pushing"
 git push --set-upstream origin master
